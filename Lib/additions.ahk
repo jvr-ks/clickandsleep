76eccc20e9435b66642a59e5e093c0ecd1f958af
Joerg von Roos <40567703+jvr-ks@users.noreply.github.com>
2023-08-17 11:02:56 +0200

    0.436

---
+++ Lib/additions.ahk
@@ -0,0 +1,21 @@
+﻿;additions.ahk
+
+; up to 6 parameters -> cascommands.ahk
+
+showTextFile(f,n){
+  theSourceCode := ""
+  if (FileExist(f) != ""){
+    f := FileOpen(f,"r")
+    theSourceCode := f.Read()
+    f.Close()
+  }
+  Gui, showTextFile:Add, Edit, w0 h0 ; focus dummy
+  Gui, showTextFile:Add, Edit, x22 y30 w440 h270 t8 t16 t24 t32 t40 t48 WantTab readonly, %theSourceCode%
+  Gui, showTextFile:Show, xCenter yCenter h379 w479, showTextFile(%f%)
+  sleep, n
+  Gui, showTextFile:Destroy
+  return
+}
+
+
+
---
+++ updater_default_files$_$_$64.bat
@@ -0,0 +1,30 @@
+@rem updater_default_files$_$_$64.bat
+
+copy /Y "clickandsleep.exe" "clickandsleep.exe.default"
+
+copy /Y "clickandsleep.ini" "clickandsleep.ini.default"
+
+copy /Y "clickandsleep.txt" "clickandsleep.txt.default"
+
+copy /Y "clickandsleep_test0.txt" "clickandsleep_test0.txt.default"
+
+copy /Y "clickandsleep_test1.txt" "clickandsleep_test1.txt.default"
+
+copy /Y "clickandsleep_test2.txt" "clickandsleep_test2.txt.default"
+
+copy /Y "clickandsleep_test3.txt" "clickandsleep_test3.txt.default"
+
+copy /Y "clickandsleep_test4.txt" "clickandsleep_test4.txt.default"
+
+copy /Y "clickandsleep_test5.txt" "clickandsleep_test5.txt.default"
+
+copy /Y "clickandsleep_test6.txt" "clickandsleep_test6.txt.default"
+
+copy /Y "clickandsleep_test7.txt" "clickandsleep_test7.txt.default"
+
+copy /Y "clickandsleep_test8.txt" "clickandsleep_test8.txt.default"
+
+copy /Y "clickandsleep_test9.txt" "clickandsleep_test9.txt.default"
+
+copy /Y "clickandsleep_test10.txt" "clickandsleep_test10.txt.default"
+
